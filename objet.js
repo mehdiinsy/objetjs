86ca2d22ef8f3647e287a4d414127535498b017a
mehdiinsy <mehdi-belkateb@hotmail.com>
2023-03-24 16:20:29 +0100

    o

---
+++ objet.js
@@ -0,0 +1,46 @@
+mehdi 
+
+//1 
+let personne = {"jojo","20ans","Paris"} ;
+console.log(personne) ;
+
+//2
+let voiture = {"nissan","gtr","1999"} ;
+//je ne sais pas afficher les details
+
+//3
+let etudiant = {"jojo","20ans","15"};
+//je ne connais pas la methode moyenne
+
+
+
+//4
+let animal = {"espece","poids","cri"};
+console.log({2});
+
+//5
+//pas compris pourquoi 2objets
+
+
+//
+let personne = {"jojo","jaja","age","france"} ;
+alert({0}+{1}+{2})
+
+
+//
+let animal = {"jojo","lion","12","pays"} ;
+let cri = "rugit"
+let especes
+if (especes={1}){
+    alert(cri)
+}
+
+
+//
+let objet = {10,20};
+alert({0}+{1});
+alert({0}-{1});
+alert({0}*{1});
+alert({0}/{1});
+
+
